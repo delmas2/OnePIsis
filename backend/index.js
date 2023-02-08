051984998951944f97e38df34320744eee93108c
Tacett <79200344+Tacett@users.noreply.github.com>
2023-02-08 16:03:52 +0100

    Séance2 - mutations

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { ApolloServer, gql } = require('apollo-server-express');
 const app = express();
+const fs = require('fs').promises;
 
 const typeDefs = require("./schema")
 const resolvers = require("./resolvers")
@@ -8,9 +9,20 @@ const resolvers = require("./resolvers")
 const server = new ApolloServer({
     typeDefs, resolvers,
     context: async ({ req }) => ({
-    world: world
+        world: await readUserWorld(req.headers["x-user"]),
+        user: req.headers["x-user"]
     })
-   });
+});
+
+async function readUserWorld(user) {
+    try {
+        const data = await fs.readFile("userworlds/" + user + "-world.json");
+    return JSON.parse(data);
+    }
+    catch (error) {
+        return world
+    }
+}
 
 let world = require("./world")
 app.use(express.static('public'));
