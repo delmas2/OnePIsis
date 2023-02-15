f5e71f32dbc9fc5070de3c0937cddf4404770c17
delmas2 <leo.delmas@etud.univ-jfc.fr>
2023-02-15 16:22:03 +0100

    suite
    suite

--- frontend/onepisis/src/index.tsx
+++ frontend/onepisis/src/index.tsx
@@ -4,12 +4,22 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {ApolloClient, ApolloProvider, InMemoryCache} from "@apollo/client";
+
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache()
+ });
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <ApolloProvider client={client}>
     <App />
+    </ApolloProvider>
+    
   </React.StrictMode>
 );
 
